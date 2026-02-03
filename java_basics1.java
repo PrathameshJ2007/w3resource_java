bbda152158746b8d0a89f8f7f90ab5e8782e05ea
Prathamesh Bapu Jagtap <prathamesh_jagtap_it@moderncoe.edu.in>
2026-02-03 11:51:20 +0530

    Rename methods for consistency and update main calls

--- java_basics1.java
+++ java_basics1.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class java_basics1 {
-    public static void Prob32_sumdjt (){
+    public static void Prob33_sumdjt (){
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         int sum = 0 ;
@@ -13,12 +13,12 @@ public class java_basics1 {
          
     }
 
-    public static void Prob33_hexArea(){
+    public static void Prob34_hexArea(){
         Scanner sc = new Scanner(System.in);
         double Slen = sc.nextDouble(); 
         System.out.println("Area of Hexagon : " + (6 * Math.pow(Slen,2))/(4 * Math.tan((Math.PI/6))));
     }
-    public static void Prob34_Area(){
+    public static void Prob35_Area(){
         Scanner sc = new Scanner(System.in);
         double sides = sc.nextDouble();
         double Slen = sc.nextDouble(); 
@@ -28,9 +28,9 @@ public class java_basics1 {
 
 
     public static void main(String arg[]){
-    //Prob32_sumdjt();
-    //Prob33_hexArea();
-    //Prob34_polArea();
+    //Prob33_sumdjt();
+    //Prob34_hexArea();
+    //Prob35_polArea();
     }
 }
 
